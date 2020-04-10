bc705cdf2a29c2ad0beee691fdcb7ed5934ab4c3
n0161264 <norma.madrid@libertymutual.com>
2020-04-10 18:24:57 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_ilbb9kQnf', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '33k23e0crcn2ogb4p9der9sc1k', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
